833d8e7b8e7c2578d8c8ac073c1b452406d794b3
Joey <toshajo99@gmail.com>
2017-10-05 23:37:22 -0400

    Created jcce_view_tool

---
+++ lib/jcce_view_tool.rb
@@ -0,0 +1,6 @@
+require "jcce_view_tool/version"
+require "jcce_view_tool/renderer"
+
+module JcceViewTool
+  # Your code goes here...
+end
---
+++ lib/jcce_view_tool/version.rb
@@ -0,0 +1,3 @@
+module JcceViewTool
+  VERSION = "0.1.0"
+end
